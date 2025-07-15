f70485aee975c61c85a0dd1b8f2c321ef9dd286f
DiasLuc <diaslucp@gmail.com>
2025-07-15 14:03:53 -0300

    Adding screenshots

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    // How to have video of execution
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
